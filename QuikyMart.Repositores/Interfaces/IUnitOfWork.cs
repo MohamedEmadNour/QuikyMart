bc04744843f9706f0be642916b26de7fd31961e3
MohamedEmadNour <MohamedEmadNOur@outlook.com>
2024-04-04 00:31:09 +0200

    IUnitOfWork Generic

---
+++ QuikyMart.Repositores/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,17 @@
+﻿using QuikyMart.Data.Entites;
+using QuikyMart.Repositories.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuikyMart.Repositores.Interfaces
+{
+    public interface IUnitOfWork 
+    {
+        IGenericRepositories<T> repositories<T>() where T : BaseEntity;
+
+        Task<int> CompleteAsync();
+    }
+}
---
+++ QuikyMart.Repositores/Repositories/UnitOfWork.cs
@@ -0,0 +1,46 @@
+﻿using QuikyMart.Data.DB.Context;
+using QuikyMart.Data.Entites;
+using QuikyMart.Repositores.Interfaces;
+using QuikyMart.Repositories.Interfaces;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuikyMart.Repositores.Repositories
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly QuikyMartDBContext _context;
+        private Hashtable _Repositorie;
+
+        public UnitOfWork(QuikyMartDBContext context)
+        {
+            _context = context;
+        }
+        public async Task<int> CompleteAsync()
+            => await _context.SaveChangesAsync();
+        
+
+        public IGenericRepositories<T> repositories<T>() where T : BaseEntity
+        {
+            if (_Repositorie == null)
+                _Repositorie = new Hashtable();
+
+            var EntityName = typeof(T).Name;
+
+            if (_Repositorie.ContainsKey(EntityName))
+            {
+                var RepositoriesType = typeof(IGenericRepositories<T>);
+
+                var RepositoriesInstance = Activator.CreateInstance(RepositoriesType.MakeGenericType(typeof(T)) , _context );
+
+                _Repositorie.Add(EntityName, RepositoriesInstance);
+
+            }
+            return (IGenericRepositories<T>) _Repositorie[EntityName];
+        }
+    }
+}
